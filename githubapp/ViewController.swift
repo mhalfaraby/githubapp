404ecbeca0e5c09e2d75e16d109bc8bf784bef17
mhalfaraby <61083129+mhalfaraby@users.noreply.github.com>
2020-10-22 12:20:47 +0700

    Update ViewController.swift

--- githubapp/ViewController.swift
+++ githubapp/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("hello word")
         // Do any additional setup after loading the view.
     }
 
